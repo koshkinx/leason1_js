87197c23b28f42a444eaf16521bcf457b8a50ccd
koshkinx <koshkin9x@gmail.com>
2022-12-02 20:23:13 +0200

    status

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 //отримати 2 цифрових значення і вивести результат математичнихх операцій в console.
-let a = 4 + 4;
-let b = 5 - 3;
-console.log(a + b);
-console.log(a / b);
+let firstValue = prompt("answer one ");
+let secondValue = prompt("answer two ");
+console.log(Number(firstValue) + Number(secondValue));
+console.log(Number(firstValue) / Number(secondValue));
 // Отримати 2 текстових значення і вивсести за допомогою document .write  "User name: FirstVar  User last name SecondVar "
 // ( firstVar, secondVar значення які отримали )
-let FirstVar = "James";
-let SecondVar = "Bond";
+let FirstVar = prompt("James");
+let SecondVar = prompt("Bond");
 document.write("User name:", FirstVar);
 document.write("</br>");
 document.write("User last name:", SecondVar);
 //отримати текстове  значення і виконати перевіку: якщо  uk повернути true, якщо ru  повернути false
-let uk = 1;
-let ru = 1;
-let trust = true;
-let goodPeople = false;
+let uk = prompt(1);
+let ru = prompt(1);
+let trust = prompt(true);
+let goodPeople = prompt(false);
 let rain = uk == trust;
 let rain1 = ru == goodPeople;
 console.log(rain);
@@ -22,7 +22,6 @@ console.log(rain1);
 //отримати 2 числових значення якщо перше більше 20 а друге більше 30 то вивстести true, якщо одна з умов не відповідає вивсести false
 let first = 21;
 let second = 31;
-console.log(first > 20, "true");
-console.log(second > 30, "true");
-console.log(first < 20, "false");
-console.log(second < 30, "false");
+console.log(first > 20 && second > 30);
+
+console.log(first < 20 && second < 30);
